34161990d062879266e7449c6c9b94f3e15d962e
Parker <contact@pkrm.dev>
2023-11-25 23:05:55 -0600

    Move to flask webserver

---
+++ app/views.py
@@ -0,0 +1,36 @@
+import flask
+import os
+import discord
+
+app = flask.Flask(__name__)
+app.secret_key = os.urandom(32)
+webhook_url = os.environ.get('WEBHOOK_URL')
+
+@app.route('/', methods=['GET'])
+def index():
+    return flask.render_template('index.html')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if flask.request.method == 'GET':
+        return flask.render_template('contact.html')
+
+    if flask.request.method == 'POST':
+        try:
+            name = flask.request.form['name']
+            email = flask.request.form['email']
+            message = flask.request.form['message']
+            # Send the contact form to Discord via a webhook
+            webhook = discord.SyncWebhook.from_url(webhook_url)
+
+            embed = discord.Embed(
+                title='New Message',
+                description=f"**Name:** ` {name} `\n**Email:** ` {email} `\n**Message:** ` {message} `",
+                color=0x85C0F7
+            )
+            webhook.send(embed=embed)
+
+            return flask.render_template('contact.html', success=True)
+        # If any error happens for any reason, return the contact page with error
+        except:
+            return flask.render_template('contact.html', error=True)
---
+++ run.py
@@ -0,0 +1,4 @@
+from app.views import app as application
+
+if __name__ == '__main__':
+    application.run(port="2121", debug=False)
